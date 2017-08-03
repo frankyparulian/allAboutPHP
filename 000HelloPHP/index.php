eb105103fd9f9d2c2fd8cc8253ac395005e1bd18
frankyparulian <frankyparulian@gmail.com>
2017-08-03 18:05:40 +0700

    Add description on code hello world program

--- 000HelloPHP/index.php
+++ 000HelloPHP/index.php
@@ -1,3 +1,10 @@
 <?php
-	echo "Hello World php";
+/*
+	author		: franky parulian silalahi
+	email		: frankyparulian@gmail.com
+	description	: This simple program will display hello world using php.
+	
+*/
+	echo "Hello World php.";
+	print "Good Luck!!!"
 ?>
